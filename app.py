72c9194927ea2955c1d9973c5afef49087dda6d8
pramod21kadam <pramod21dkadam@gmail.com>
2020-09-20 15:11:50 +0530

    make it better

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from packages.packages import *
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000)
-    # socketio.run(app)
+    # socketio.run(app, host='0.0.0.0', port=5000)
+    socketio.run(app)
     # app.run(debug = True)
     # app.run(host='192.168.43.209', port="5000")
     # app.run(debug=True, ssl_context = ('cert.pem', 'key.pem'), host='192.168.43.209', port=5000)
