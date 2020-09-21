988a9242dc6a7572c1923a1273f148c20712bcc2
pramod21kadam <pramod21dkadam@gmail.com>
2020-09-21 16:44:37 +0530

    branch

--- socket_and_routing/routing.py
+++ socket_and_routing/routing.py
@@ -5,4 +5,6 @@ app = init.app
 
 @app.route('/', methods = ['GET'])
 def index():
+    if "manager_1234" in request.args and request.args["manager_1234"] == "123@Iron":
+        return render_template('manage.html', title="Manager")
     return render_template('dashboard.html', title="Talk To Strangers")
